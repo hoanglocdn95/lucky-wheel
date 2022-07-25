d34c3770ddb2ebbc6441629d4d1a950f38806f9b
locth-1708 <49936698+locth-1708@users.noreply.github.com>
2022-07-25 22:28:20 +0700

    create simple lucky wheel

--- src/App.js
+++ src/App.js
@@ -1,14 +1,64 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [isRotating, setRotating] = useState(false);
+  const items = [
+    {
+      content: 'Yes',
+      color: 'red',
+    },
+    {
+      content: 'No',
+      color: 'blue',
+    },
+    {
+      content: 'Not Given',
+      color: 'green',
+    },
+    {
+      content: 'Given',
+      color: 'yellow',
+    },
+  ];
+  const calculateDeg = () => Math.floor(Math.random() * 360 + 720);
+
   const renderWheelItem = () => {
-    return <div className="wheel__item"></div>;
+    return items.map((item, index) => {
+      return (
+        <div
+          key={index}
+          className="wheel__item"
+          style={{
+            transform: `rotate(${(360 / items.length) * index}deg)`,
+            borderTopColor: item.color,
+            borderWidth: `175px ${350 / (items.length / 2)}px 0`,
+          }}
+        >
+          <p>{item.content}</p>
+        </div>
+      );
+    });
   };
   return (
     <div className="layout">
       <div className="container">
-        <div className="wheel">{renderWheelItem()}</div>
+        <div className="rotate" onClick={() => setRotating(!isRotating)}>
+          ROTATE
+        </div>
+        <div className="indicator"></div>
+        <div
+          className="wheel"
+          style={
+            isRotating
+              ? {
+                  transform: `rotate(${calculateDeg()}deg)`,
+                }
+              : {}
+          }
+        >
+          {renderWheelItem()}
+        </div>
       </div>
     </div>
   );
